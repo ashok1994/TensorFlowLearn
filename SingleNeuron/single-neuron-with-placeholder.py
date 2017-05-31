272e0ecc0f9cf39e0889112e02789a0b3f0fdffb
Ashok Mandal <ashok1994>
2017-05-31 21:48:53 +0530

    Added placeholder variable | fixed learning rate

--- SingleNeuron/single-neuron-with-placeholder.py
+++ SingleNeuron/single-neuron-with-placeholder.py
@@ -1,15 +1,17 @@
 # Suppress OS related warnings
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-from random import uniform
+import sys
 
 # Import tensorflow library
 import tensorflow as tf
+import numpy as np
+
 sess = tf.Session()
 
-# Input Data X : of Constant Value 1.0 tf.float32
-x = tf.random_uniform([])
+
+# Input Data X : of placeholder Value 1.0 tf.float32
+x = tf.placeholder(tf.float32, name="input")
 
 # Variable Weight : Arbitary Value
 w = tf.Variable(0.8, name='weight')
@@ -27,9 +29,9 @@ with tf.name_scope("loss"):
 
 # Training Step : Algorithm -> GradientDescentOptimizer
 with tf.name_scope("training"):
-    train_step = tf.train.GradientDescentOptimizer(0.025).minimize(loss)
+    train_step = tf.train.GradientDescentOptimizer(0.00001).minimize(loss)
 
-# Ploting graph : Tensorboard
+# # Ploting graph : Tensorboard
 for value in [x, w, y, actual_output, loss]:
     tf.summary.scalar(value.op.name, value)
 
@@ -43,8 +45,9 @@ summary_writer = tf.summary.FileWriter('log_simple_stats', sess.graph)
 sess.run(tf.global_variables_initializer())
 
 for i in range(300):
-    summary_writer.add_summary(sess.run(summaries), i)
-    sess.run(train_step)
+    sample = np.random.uniform(low=0.0, high=300.0)
+    _, merged = sess.run([train_step, summaries], feed_dict={x: sample})
+    summary_writer.add_summary(merged, i)
 
 # Output
 print(sess.run([w]))
